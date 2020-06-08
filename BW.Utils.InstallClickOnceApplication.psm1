8a2ec715a45b02fede28fe5b0f9262d361bc5ef9
Shannon Graybrook <shannon.graybrook@gmail.com>
2020-06-08 13:03:46 -0500

    initial commit

---
+++ BW.Utils.InstallClickOnceApplication.psm1
@@ -0,0 +1,106 @@
+﻿using namespace System.Deployment.Application
+
+function Install-ClickOnceApplication {
+    # .ExternalHelp BW.Utils.InstallClickOnceApplication-help.xml
+
+    [CmdletBinding()]
+    param(
+
+        [Parameter( Mandatory, Position=1 )]
+        [Alias('Uri')]
+        [uri]
+        $ManifestUri,
+
+        [switch]
+        $Elevate,
+
+        [int]
+        $ManifestTimeout = 15,
+
+        [int]
+        $DownloadTimeout = 60
+
+    )
+
+    begin {
+
+        Add-Type -AssemblyName System.Deployment
+
+        Write-Verbose 'Starting installation of ClickOnce application manifest'
+        Write-Verbose ( 'Manifest URI: ' + $ManifestUri.AbsoluteUri )
+
+        $HostingManager = New-Object InPlaceHostingManager -ArgumentList $ManifestUri, $false
+
+        # register event for manifest download completion
+        Register-ObjectEvent -InputObject $HostingManager -EventName GetManifestCompleted -Action {
+            
+            Write-Information ( 'Processing manifest for {0}' -f $Event.SourceEventArgs.ProductName ) -InformationAction Continue
+
+            New-Event -SourceIdentifier 'ManifestDownloadComplete'
+        
+        } > $null
+
+        # register event for application download completion
+        Register-ObjectEvent -InputObject $HostingManager -EventName DownloadApplicationCompleted -Action {
+
+            New-Event -SourceIdentifier 'DownloadApplicationCompleted'
+        
+        } > $null
+
+    }
+
+    process {
+
+        Write-Verbose 'Starting manifest download...'
+
+        # start manifest download
+        $HostingManager.GetManifestAsync()
+    
+        # wait for download to complete
+        $Event = Wait-Event -SourceIdentifier 'ManifestDownloadComplete' -Timeout $ManifestTimeout
+
+        if ( -not $Event ) {
+
+            Write-Error ( 'ClickOnce manifest download timed out after {0} seconds' -f $ManifestTimeout )
+            return
+
+        }
+
+        $Event | Remove-Event
+
+        # elevate installation if needed
+        $HostingManager.AssertApplicationRequirements( $Elevate )
+            
+        Write-Verbose 'Manifest download complete, beginning application download...'
+
+        # start application download
+        $HostingManager.DownloadApplicationAsync()
+            
+        # wait for download to complete
+        $Event = Wait-Event -SourceIdentifier 'DownloadApplicationCompleted' -Timeout $DownloadTimeout
+
+        if ( -not $Event ) {
+
+            Write-Error ( 'Application download timed out after {0} seconds' -f $DownloadTimeout )
+            return
+
+        }
+
+        $Event | Remove-Event
+
+        Write-Verbose 'Application download completed'
+    
+    }
+    
+    end {
+    
+        Get-EventSubscriber |
+            Where-Object { $_.SourceObject.ToString() -eq 'System.Deployment.Application.InPlaceHostingManager' } |
+            Unregister-Event
+
+        $HostingManager.Dispose()
+    
+    }
+
+}
+
--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # BW.Utils.InstallClickOnceApplication
-Small utility module to handle installing ClickOnce apps
+Small utility module to handle installing ClickOnce apps. Most of the code
+comes from a script by [Jonathon Chupinsky](https://techcommunity.microsoft.com/t5/user/viewprofilepage/user-id/147200)'s
+[post](https://techcommunity.microsoft.com/t5/office-365/export-to-pst-via-powershell/m-p/95007)
+on Microsoft Tech Community. I have just reformatted and packaged the code
+for PSGallery.
+
+# Usage
+```powershell
+PS C:\> $ManifestURI = 'https://complianceclientsdf.blob.core.windows.net/v16/Microsoft.Office.Client.Discovery.UnifiedExportTool.application'
+PS C:\> Install-ClickOnceApplication $ManifestURI -Elevate
+```
